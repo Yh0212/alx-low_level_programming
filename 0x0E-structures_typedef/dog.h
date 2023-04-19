fd4577122660a47fa644babf916e22df4bd653dd
Yh0212 <yassinehadri84@gmail.com>
2023-04-18 19:45:01 -0700

    git fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,26 @@
-#ifndef DOH_H
-#define DOH_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - some info about dog
- * @name:First member
- * @age: Second member
- * @owner: Third member
- *
- * dog_t - typdef for struct dog
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner:  owner
+ * Description: This struct is for dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-#endif
+#endif /*DOG_H*/
+
